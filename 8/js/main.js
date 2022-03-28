7a11c668f6f9280bc2f90fc2b81c58bf0a08ce49
Keks <keks@htmlacademy.ru>
2022-03-28 12:25:37 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import './data-photos.js';
+import './users.js';
+import './big-photo.js';
+import './upload-photo-form.js';
+import './validate-hashtag.js';
+import './scale-upload-photo.js';
+import './effects.js';
